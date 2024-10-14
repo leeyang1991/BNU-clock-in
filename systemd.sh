1d3bc0b504a7641aec52884363e9548514f4e551
Yang Li <yangligeo@arizona.edu>
2024-10-14 11:24:40 -0700

    add json

--- systemd.sh
+++ systemd.sh
@@ -1,3 +1,5 @@
+wget https://raw.githubusercontent.com/leeyang1991/BNU-clock-in/refs/heads/master/multiple_server_ping.py -O /root/network_test.py
+
 echo "[Unit]
 Description=Network test
 After=network.target
@@ -8,5 +10,7 @@ ExecStart=/usr/bin/python3 /root/network_test.py
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/network_test.service
-systemctl start network_test.service
+
+systemctl restart network_test.service
 systemctl enable network_test.service
+systemctl status network_test.service
