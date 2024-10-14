693943250eb9fd716c8cc7719796d06f4f550443
Yang Li <yangligeo@arizona.edu>
2024-10-14 11:43:45 -0700

    max_fail_times = 2

--- systemd.sh
+++ systemd.sh
@@ -1,4 +1,3 @@
-wget https://raw.githubusercontent.com/leeyang1991/BNU-clock-in/refs/heads/master/multiple_server_ping.py -O /root/network_test.py
 
 echo "[Unit]
 Description=Network test
@@ -10,6 +9,8 @@ ExecStart=/usr/bin/python3 /root/network_test.py
 
 [Install]
 WantedBy=multi-user.target" > /etc/systemd/system/network_test.service
+
+wget https://raw.githubusercontent.com/leeyang1991/BNU-clock-in/refs/heads/master/multiple_server_ping.py -O /root/network_test.py
 
 systemctl restart network_test.service
 systemctl enable network_test.service
